768ab4075324443073af1670bc3b3c581256843e
Adrianzzziny <adri-escobar@hotmail.com>
2022-03-11 08:32:26 -0500

    Ejercicios

--- 08-Algoritmos/08-Algoritmos.js
+++ 08-Algoritmos/08-Algoritmos.js
@@ -5,7 +5,16 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-  
+  for (let i = 0; i < array.length; i++) {
+    for(let j = 0; j < array.length-1; j++){
+    if(array[j]>array[j+1]){
+      let bubble = array[j]
+      array[j] = array[j+1]
+      array[j+1] = bubble 
+    }
+  }
+}
+  return array
 };
 
 
@@ -15,9 +24,17 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
- 
-
+  for (let i = 1; i < array.length; i++) {
+    let aux=array[i]
+  for(let j = 0; j < i; j++){
+    if (aux<array[j]){
+      aux=array[j]
+      array[j]=array[i]
+      array[i]=aux
+    }
+  }  
+}
+ return array
 }
 
 
@@ -26,8 +43,22 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
-
+  for (let i = 0; i < array.length; i++) {
+    let min = array[i]
+    let current = i
+   for (let j = i+1; j < array.length; j++) {
+     if (min>array[j]) {
+       min = array[j]
+       current = j
+     }
+     
+   } 
+   if (current!==i) {
+     array[current] = array[i]
+     array[i]=min
+   }
+  }
+  return array
 }
 
 
